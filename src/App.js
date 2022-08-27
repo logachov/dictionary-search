47b5c0fe63cb7ca72e3f1e1e4de5f9f178740b05
Olesia <102444640+logachov@users.noreply.github.com>
2022-08-27 11:11:49 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo-fox.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <img src={logo} className="logo img-fluid" alt="logo" />
+
+    </div>
+  );
+}
+
+export default App;
